def999b9a69fe76263fb1b816f656849bc8d95ff
Pooja S Ganti <ganti@codeaurora.org>
2017-12-17 20:23:35 +0530

    bootable/recovery: Add mode to open() syscall
    Change-Id: I58d0a973b269397a2a15ae47ec968a8986a9edca
    Signed-off-by: Akshaya Bhat <akshayab@codeaurora.org>

--- recovery.cpp
+++ recovery.cpp
@@ -413,7 +413,7 @@ static void copy_logs() {
 static int set_ota_cookie() {
     int fd = -1;
     int rcode = 0;
-    fd = open(STATUS_COOKIE_FILE, O_CREAT | O_RDWR);
+    fd = open(STATUS_COOKIE_FILE, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
     if (fd < 0) {
         LOGE("Failed to open %s : %s\n",
              STATUS_COOKIE_FILE,
