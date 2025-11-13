41ec291bb73e5a6fdc5bd3b51e445f71952d2896
Shaoxiang Wang <shaoxian@qti.qualcomm.com>
2025-11-13 10:51:04 +0800

    Recovery: Added the feature of upgrading via USB disk.
    Change-Id: I5f88791b93b027f6471ac43bbf8b45195cdcada0
    Signed-off-by: Shaoxiang Wang <shaoxian@qti.qualcomm.com>

--- recovery_utils/roots.cpp
+++ recovery_utils/roots.cpp
@@ -386,7 +386,7 @@ int setup_install_mounts() {
   for (const FstabEntry& entry : fstab) {
     // We don't want to do anything with "/".
     if (entry.mount_point == "/" || entry.mount_point == "/sdcard" ||
-        entry.mount_point == "/data") {
+        entry.mount_point == "/data" || entry.mount_point == "/usbdisk") {
       continue;
     }
 
